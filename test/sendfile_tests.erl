9d1ff33b7ee49757389a3cb114aa6cacb2cf1c16
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2011-08-04 18:51:06 +0200

    Use proper EUnit filename

--- test/sendfile_tests.erl
+++ test/sendfile_tests.erl
@@ -5,7 +5,7 @@
 %%% Use of this source code is governed by a BSD-style
 %%% license that can be found in the LICENSE file.
 
--module(sendfile_test).
+-module(sendfile_tests).
 -author(tuncerayaz).
 -export([server/1]).
 
